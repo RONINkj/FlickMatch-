4e19aca08e9ee6d093f2e577f7b171d983ce909a
Kanishk Jaiswal <umangkj12@gmail.com>
2025-11-28 17:21:20 +0530

    update main.py in backend

--- FlickMatch/app/main.py
+++ FlickMatch/app/main.py
@@ -21,11 +21,16 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:4173",
+        "http://127.0.0.1:4173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 # Load recommender on startup
 recommender: RecommenderModel = load_recommender(
